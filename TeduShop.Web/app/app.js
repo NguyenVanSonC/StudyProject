508a06698b1c19d2abb3f138d9bccc57060a9c44
SonManu <nguyensond15@gmail.com>
2019-02-15 18:51:08 +0700

    set layout

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('tedushop', ['tedushop.products', 'tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/componnents/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
